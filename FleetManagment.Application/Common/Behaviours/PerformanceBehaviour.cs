5a5feca9e874b5998c2ac7a8cb2733cc6661ba4b
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2022-12-19 13:40:25 +0100

    Behaviours cd.

--- FleetManagment.Application/Common/Behaviours/PerformanceBehaviour.cs
+++ FleetManagment.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FleetManagment.Application.Common.Behaviours
 {
-    internal class PerformanceBehaviour
+    public class PerformanceBehaviour<TRequest, TResponce> : IPipelineBehavior<TRequest, TResponce> where TRequest : IRequest<TResponce>
     {
+        private readonly ILogger _logger;
+        private readonly Stopwatch _timer;
+        public PerformanceBehaviour(ILogger<TRequest> logger)
+        {
+            _timer = new Stopwatch();
+            _logger = logger;
+        }
+
+        public async Task<TResponce> Handle(TRequest request, RequestHandlerDelegate<TResponce> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsed = _timer.ElapsedMilliseconds;
+
+            if (elapsed > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+
+                _logger.LogInformation("MyRentalShop Request: {Name} ({elapsed} milliseconds) {@Request}", requestName, elapsed, request);
+            }
+
+            return response;
+        }
     }
+
 }
