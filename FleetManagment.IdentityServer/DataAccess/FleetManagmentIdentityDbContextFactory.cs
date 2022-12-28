4b227167d39573e2517f1878f3cacb6bbb12b500
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2022-12-28 13:45:05 +0100

    Add DesignTimeDbContextFactory

---
+++ FleetManagment.IdentityServer/DataAccess/FleetManagmentIdentityDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagment.IdentityServer.DataAccess
+{
+    public class FleetManagmentIdentityDbContextFactory : DesignTimeDbContextFactoryBase<FleetManagmentIdentityDbContext>
+    {
+        protected override FleetManagmentIdentityDbContext CreateNewInstance(DbContextOptions<FleetManagmentIdentityDbContext> options)
+        {
+            return new FleetManagmentIdentityDbContext(options);
+        }
+    }
+}
