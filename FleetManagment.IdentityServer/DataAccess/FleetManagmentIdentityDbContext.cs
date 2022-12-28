584a394d484a74c7d415b4e2507b5735399b5083
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2022-12-28 09:04:00 +0100

    Add FleetManagmentIdentityDbContext

--- FleetManagment.IdentityServer/DataAccess/FleetManagmentIdentityDbContext.cs
+++ FleetManagment.IdentityServer/DataAccess/FleetManagmentIdentityDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace FleetManagment.IdentityServer.DataAccess
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagment.IdentityServer.DataAccess
 {
-    public class FleetManagmentIdentityDbContext
-    {
+    public class FleetManagmentIdentityDbContext : IdentityDbContext
+    { 
+        public FleetManagmentIdentityDbContext(DbContextOptions<FleetManagmentIdentityDbContext> options) : base(options)
+        {
+
+        }
     }
 }
--- FleetManagment.IdentityServer/Program.cs
+++ FleetManagment.IdentityServer/Program.cs
@@ -1,4 +1,6 @@
+using FleetManagment.IdentityServer.DataAccess;
 using IdentityServer4.Test;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly.GetName().Name;
 var connectionString = builder.Configuration.GetConnectionString("FleetManagmentDbContext");
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<FleetManagmentIdentityDbContext>();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
