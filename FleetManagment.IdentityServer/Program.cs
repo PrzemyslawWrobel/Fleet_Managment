3c67187467294b10ba5f9a6f9576a3d64ab02090
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2022-12-29 09:42:30 +0100

    Add UI

--- FleetManagment.IdentityServer/Program.cs
+++ FleetManagment.IdentityServer/Program.cs
@@ -1,32 +1,51 @@
+using FleetManagment.IdentityServer;
 using FleetManagment.IdentityServer.DataAccess;
 using IdentityServer4.Test;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
-var connectionString = builder.Configuration.GetConnectionString("FleetManagmentDbContext");
+var defaultConnString = builder.Configuration.GetConnectionString("FleetManagmentDbContext");
+
+
+builder.Services.AddDbContext<FleetManagmentIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString,
+        b => b.MigrationsAssembly(assembly)));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<FleetManagmentIdentityDbContext>();
-
 builder.Services.AddIdentityServer()
     .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
-        options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
-            sql => sql.MigrationsAssembly(assembly));
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
     })
     .AddOperationalStore(options =>
     {
-        options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
-            sql => sql.MigrationsAssembly(assembly));
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
     })
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+//app.UseAuthorization();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapDefaultControllerRoute();
+//});
 
 app.Run();
