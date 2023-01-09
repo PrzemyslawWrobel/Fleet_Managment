0573a430bde0a512f5efe81fddff27f9306ef4f5
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2023-01-09 12:09:23 +0100

    Interfaces move to shared models

--- FleetManagment.SharedModels/Customers/CustomerDto.cs
+++ FleetManagment.SharedModels/Customers/CustomerDto.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿
+using AutoMapper;
 using FleetManagment.Application.Common.Interfaces;
 using FleetManagment.Domain.Entities;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace FleetManagment.Application.Customers.Queries.GetCustomers
 {
---
+++ FleetManagment.SharedModels/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagment.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string Email { get; set; }
+        bool IsAuthenticated { get; set; }
+    }
+}
