bb14b7849512f4662975a081a7f818fa14949d2b
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2023-01-09 10:56:57 +0100

    Add project SharedModels for Blazor

---
+++ FleetManagment.Client/Pages/Customers.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FleetManagment.Client.Pages
+{
+    public partial class Customers
+    {
+        //private List<CustomersModel> customers = new List<CustomersModel>;
+    }
+}
